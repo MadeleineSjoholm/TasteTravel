1787d27db1e7f560d80bbfdce0423f4b2dc4ac23
AlvaB-ux <Alva.Blomkvist.9444@student.uu.se>
2021-04-23 14:00:42 +0200

    big map

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,19 +7,14 @@ const Hero = () =>
   <section className="hero is-default is-bold">
     <div className="hero-body">
       <div className="container">
-        <div className="columns is-vcentered">
-          <div className="column is-5 is-offset-1 landing-caption">
             <h1 className="title is-1 is-bold is-spaced">
                 Travel with your taste buds.
             </h1>
-            <MapIndex />
+            <MapIndex className= 'map'/>
             <a className="button cta rounded primary-btn raised">
                   Get Started
               </a>
-           
           </div>
-        </div>
-      </div>
     </div>
   </section>
 
