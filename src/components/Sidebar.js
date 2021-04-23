2f93459f5625a566e7a4fb086176d9623a5cb455
AlvaB-ux <Alva.Blomkvist.9444@student.uu.se>
2021-04-23 15:52:00 +0200

    kommenterat ut helpknappen i sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -18,19 +18,20 @@ const Sidebar = () => {
         <button className="category-sidebar-buttons">
         <li><span className="nav-section-title"></span></li>
          <a href="/preferences"> PREFERENCES </a><img src="preferences.png" width="35" height="35" alt='description'/>
-        </div>
+        </button>
 
         <button className="category-sidebar-buttons">
         <li><span className="nav-section-title"></span></li>
         <a href="/mostPopular"> MOST POPULAR</a> <img src="stars.png" width="40" height="40" alt='description' />
-        </div>
+        </button>
 
         <button className="category-sidebar-buttons">
         <li><span className="nav-section-title"></span></li>
         <a href="/savedRecipes"> SAVED RECIPES </a> <img src="heart.png" width="40" height="40" alt='description' /><br/>
-        </div>
+        </button>
 
 
+{/*
           <li className="have-children"><a href="#"><img src="help.png" width="20" height="20" alt='description' /><br/>HELP</a>
             <ul>
               <li><a href="#">Account settings</a></li>
