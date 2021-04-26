b6ca7ddfbeb51ed69052f8f9e61e10bd5a49fc81
Madeleine Sjöholm <madeleine@sjoholm.se>
2021-04-26 15:15:15 +0200

    karta

--- src/pages/OnBoard.js
+++ src/pages/OnBoard.js
@@ -1,8 +1,21 @@
+/* eslint jsx-a11y/anchor-is-valid: 0 */
+
 import React from 'react'
+
+//import React, { useState } from 'react'
+import useForm from 'react-hook-form'
+import { useToasts } from 'react-toast-notifications'
+import { Redirect } from 'react-router-dom'
+import onlyGuest from 'components/hoc/onlyGuest'
+
 
 const OnBoard = () => {
   return (
+
     <div className="container">
+      <figure className="map_background">
+        <img src="map.png" alt="Company Logo" />
+      </figure>
       <div className="content-wrapper">
         <h1 className="title">I am OnBoard page</h1>
         <p>
@@ -15,5 +28,3 @@ const OnBoard = () => {
 
 
 export default OnBoard
-
-
