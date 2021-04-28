b9c82c7fd55b978644608282131ce714af7c74b2
Madeleine Sjöholm <madeleine@sjoholm.se>
2021-04-28 16:57:04 +0200

    typ inget på profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,33 @@
 
 import React from 'react'
+import { Link } from 'react-router-dom'
+import RegisterForm from 'components/auth/RegisterForm'
+import { register } from 'actions'
+import { useToasts } from 'react-toast-notifications'
+import onlyGuest from 'components/hoc/onlyGuest'
 
-const Profile = () => <h1> Profile Page</h1>
+const Profile = () => {
 
+  return (
+      <div className="ProfilePage">
+        <figure className="map_background">
+          <img src="map.png" alt="Company Logo" />
+        </figure>
+        <div className="container">
+          <div className="content-wrapper">
+            <h1 className="title">Profile</h1>
+
+            <div className="box">
+              <figure className="avatar">
+                <img src="logga.png" alt="Company Logo" />
+              </figure>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+  )
+}
 
 export default Profile
