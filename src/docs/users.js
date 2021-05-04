61823c9d1e421643817d669f67aafe974c658cce
FeliciaOstling <felicia.ostling@hotmail.com>
2021-05-04 09:39:31 +0200

    Början på mock-data

---
+++ src/docs/users.js
@@ -0,0 +1,10 @@
+[{"Your email":"ifernley0@cbslocal.com","Full name":"Ignacius Fernley","Avatar":"https://robohash.org/magnamsequivoluptatem.png?size=50x50&set=set1","Password ":"O3GgTr7"},
+{"Your email":"fblumire1@answers.com","Full name":"Fraze Blumire","Avatar":"https://robohash.org/nesciuntconsequatursed.png?size=50x50&set=set1","Password ":"DbbR0oVoP"},
+{"Your email":"koconnolly2@skype.com","Full name":"Kippar O'Connolly","Avatar":"https://robohash.org/recusandaequospariatur.png?size=50x50&set=set1","Password ":"5g6i9O"},
+{"Your email":"edeboick3@zimbio.com","Full name":"Elsworth Deboick","Avatar":"https://robohash.org/sedfugaconsectetur.png?size=50x50&set=set1","Password ":"H3lEkx"},
+{"Your email":"akrolak4@vimeo.com","Full name":"Annecorinne Krolak","Avatar":"https://robohash.org/eaqueetvelit.png?size=50x50&set=set1","Password ":"cA7GYWEP"},
+{"Your email":"atoy5@yandex.ru","Full name":"Adler Toy","Avatar":"https://robohash.org/molestiasquoincidunt.png?size=50x50&set=set1","Password ":"0NJKeVPX"},
+{"Your email":"mbedome6@dagondesign.com","Full name":"Megen Bedome","Avatar":"https://robohash.org/quiestquidem.png?size=50x50&set=set1","Password ":"t7CwQ6cf"},
+{"Your email":"afreund7@163.com","Full name":"Aldon Freund","Avatar":"https://robohash.org/autdeseruntdolor.png?size=50x50&set=set1","Password ":"wiB3YiT57Mu"},
+{"Your email":"millingworth8@bizjournals.com","Full name":"Michel Illingworth","Avatar":"https://robohash.org/quaerepudiandaeaut.png?size=50x50&set=set1","Password ":"nLsULka"},
+{"Your email":"mbrassington9@netscape.com","Full name":"Muhammad Brassington","Avatar":"https://robohash.org/etexut.png?size=50x50&set=set1","Password ":"9MMdBtR"}]
