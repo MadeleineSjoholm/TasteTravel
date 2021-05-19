81cc411f94fd7a3b889a59e6917f415d14e092b3
Madeleine Sjöholm <madeleine@sjoholm.se>
2021-05-19 17:05:41 +0200

    ett försök till design

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const Footer = () => (
   <div class="footer">
-  <img src="nylogg.png" alt='logo' />
 
+
+    <img src="nylogg.png" alt='logo' />
 
   <div className="team">
   <a href="/OurTeam">OUR TEAM</a>
@@ -18,6 +19,10 @@ const Footer = () => (
 
         <div className="follow">
           <h1>FOLLOW US</h1>
+          <img className="follow" src="facebook.png" alt='logo' width="200" height="300" />
+          <img className="follow" src="instagram.png" alt='logo' width="200" height="300" />
+          <img className="follow" src="youtube.png" alt='logo' width="200" height="300" />
+          <img className="follow" src="twitter.png" alt='logo' width="200" height="300" />
           </div>
 
 
