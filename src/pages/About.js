0159fcd107ab304674d5be4b1f78f14bcd83cbbb
AlvaB-ux <Alva.Blomkvist.9444@student.uu.se>
2021-05-24 15:27:27 +0200

    about us och our team

--- src/pages/About.js
+++ src/pages/About.js
@@ -21,25 +21,24 @@ const About = ({auth}) => {
         the user will then be presented with a recipe from that country. The system will, through this, offer a personalized and interactive experience, allowing the 
         user to travel with their taste buds.
 
-<br></br>
-<br></br>
+<br />
+<br />
 
         <em>Why do we use it?</em>
 <p></p>
         This past year has seen a lot of big changes in the way we live and our mobility due to the ongoing pandemic. Another tangible consequence of the pandemic
         has been our lack of possibility to travel and explore the world. When not being able to explore the world like before, you need to find new ways of
         taking part in culinary experiences, explore new cultures along with seeking new cuisines. 
-
-<br></br>
-<br></br>
+        <br />
+        <br />
 
         In combination, as an effect of our limited mobility and spending more time at home - a surge in interest has been created to explore new recipes and 
         food cultures. The survey we conducted  with 55 people showed that 75% of people asked, worked either fully or partly remote, with a majority of people 
         now cooking all or almost all meals from home. The survey showed that 76% of those surveyed have a high interest in traveling and when traveling 82% said 
         that taking part in the food culture was very important to them. 
 
-<br></br>
-<br></br>
+        <br />
+        <br />
 
         </p>
     </div>
--- src/pages/OurTeam.js
+++ src/pages/OurTeam.js
@@ -3,112 +3,54 @@ import React from 'react'
 const OurTeam = ({auth}) => {
 
   return (
+    <div className= "container">
+      <br />
+      <h1 className="title">Our Team</h1>
+        <p> The team behind TasteTravel consist of four members. </p> <br></br>
+        <div className = "member">
+          <div className="member1">
+            <img src="./TEAM/Madeleine.png" alt='pic'/>
+              <div className="person"> Madeleine </div><br />
+              <p>
+              This is Madeleine, she is a frontend developer and
+              UI designer. She is also TasteTravels own security expert. 
+              <br />
+              </p>
+          </div>
 
-<div className= "container">
+          <div className= "member2" >
+            <img src="./TEAM/moana.png" alt='pic'/>
+            <div className="person"> Moana </div><br />
+            <p>
+            <br />
+            </p>
+          </div>
 
-<br></br>
-<h1 className="title">Our Team</h1>
-<p> The team behind TasteTravel consist of four members. </p> <br></br>
+          <div className= "member3" >
+            <img src="./TEAM/alva.png" alt='pic'/>
+            <div className="person"> Alva </div><br />
+            <p>
+              The third team member is Alva who 
+              is a backend developer and also 
+              responsible for the usability 
+              and privacy of the application. 
+              <br />
+              </p>
+          </div>
 
-
-
-<div className = "member">
-
-<div className="member1">
-<img src="./TEAM/Madeleine.png" alt='pic'/>
-<div className="person"> Madeleine </div><br></br>
-<p>
-
-här text om personen och dess roll.....
-
-......
-
-
-.......
-
-
-.....
-
-
-....
-<br></br>
-</p>
-</div>
-
-<div className= "member2" >
-<img src="./TEAM/moana.png" alt='pic'/>
-<div className="person"> Moana </div><br></br>
-<p>
-
-här text om personen och dess roll.....
-
-......
-
-
-.......
-
-
-.....
-
-
-....
-<br></br>
-</p>
-
-
-</div>
-
-<div className= "member3" >
-<img src="./TEAM/alva.png" alt='pic'/>
-<div className="person"> Alva </div><br></br>
-<p>
-  
-här text om personen och dess roll.....
-
-......
-
-
-.......
-
-
-.....
-
-
-....
-<br></br>
-</p>
-
-</div>
-
-<div className= "member4" >
-<img src="./TEAM/felicia.png" alt='pic'/>
-<div className="person"> Felicia </div><br></br>
-<p>
-
-här text om personen och dess roll.....
-
-......
-
-
-.......
-
-
-.....
-
-
-....
-<br></br>
-</p>
-
-</div>
-
-</div>
-
-
-
-    </div>
-
-
+          <div className= "member4" >
+            <img src="./TEAM/felicia.png" alt='pic'/>
+            <div className="person"> Felicia </div><br />
+              <p>
+              Next up is Felicia who is a frontend developer 
+              and TasteTravels Communication Manager. 
+              Felicia is also responsible for generating the test data. 
+              <br />
+              </p>
+            </div>
+          </div>
+          <br />
+        </div> 
   )
 }
 
