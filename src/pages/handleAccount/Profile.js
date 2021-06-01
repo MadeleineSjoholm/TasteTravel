21500f2d54548b86b7d27ff7327765be8ac598c4
moanasod <73820150+moanasod@users.noreply.github.com>
2021-06-01 22:34:43 +0200

    Indent and goodbye img

--- src/pages/handleAccount/Profile.js
+++ src/pages/handleAccount/Profile.js
@@ -38,7 +38,7 @@ const Profile = (props) => {
       </figure>
       <div className="container">
         <div className="content-wrapper">
-        <h3 className="title">Your Profile</h3>
+          <h3 className="title">Your Profile</h3>
           <div class="profilepage">
 
             <div className="picture">
@@ -55,33 +55,33 @@ const Profile = (props) => {
                 <p className="subtitle has-text-grey"><b> User ID: </b>{`${userid.user.uid}`}</p>
                 <h2 className="subtitle2" > <b>Preferences </b> </h2>
                 <h3 className="subtitle has-text-grey">
-                  <ul><b> Diet : </b>{diet}</ul><br/>
-                  <ul><b> Ingredients : </b>{ingredient}</ul><br/>
-                  <ul><b>Intolernces : </b>{intolerance}</ul><br/>
+                  <ul><b> Diet : </b>{diet}</ul><br />
+                  <ul><b> Ingredients : </b>{ingredient}</ul><br />
+                  <ul><b>Intolernces : </b>{intolerance}</ul><br />
                 </h3>
 
                 <h1 className="subtitle2"><b>Settings </b></h1>
                 <p className="subtitle has-text-grey"><em>Want to update your password?</em>
 
-                <Link
-                  to="/ChangePassword">
-                  <button className="PasswordButton">
-                    Update Password
+                  <Link
+                    to="/ChangePassword">
+                    <button className="PasswordButton">
+                      Update Password
                   </button>
 
-                </Link></p>
+                  </Link></p>
                 <p className="subtitle has-text-grey"><em>Want to update your Preferences?</em>
-                <Link
-                  to="/Preferences">
-                  <button className="UpdateButton">
-                    Update Preferences
+                  <Link
+                    to="/Preferences">
+                    <button className="UpdateButton">
+                      Update Preferences
                   </button>
-                </Link></p>
+                  </Link></p>
 
                 <p className="subtitle has-text-grey">
                   <em>If you want to save recipes, start by clicking here!
                   <br /> You can also reset your saved recipes by clicking on the button below!</em>
-                <button onClick={createForm} className="SavedButton">
+                  <button onClick={createForm} className="SavedButton">
                     Set Saved Recipes
                   </button> </p>
                 <br />
